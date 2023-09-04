2f2188ab27de9a88caeb6df84da55dcdba407542
MINGW64 <kairylnisapamiry@gmail.com>
2023-09-04 16:49:46 +0600

    commit me

--- k.js
+++ k.js
@@ -1,13 +1,28 @@
+//querySelector() Document метод querySelector()
+// возвращает первый элемент ( Element ) документа,
+// который соответствует указанному селектору или 
+//группе селекторов. Если совпадений не найдено,
+// возвращает значение null 
+
+//Стандартный метод querySelector() позволет получить 
+//ссылку на конкретный элемент в документе. 
+
+//кнопок для сложения и вычитания.
+
 let counterDisplayElem = document.querySelector('.counter-display');
-let counterMinusElem = document.querySelector('.counter-minus');  //кнопок для сложения и вычитания.
+let counterMinusElem = document.querySelector('.counter-minus');  
 let counterPlusElem = document.querySelector('.counter-plus');
 
 let count = 0; 
-//Это будет переменная, которую я  буду добавлять или вычитать.
+//Это будет переменная, которую я 
+// буду добавлять или вычитать.
 
 updateDisplay();
 
-counterPlusElem.addEventListener("click",()=>{ // мне нужна будет добавить ПРОСЛУШИТЕЛИ чтобыя могла определять, что происходит, когда я нажимаю кнопка добавления или вычитания
+// мне нужна будет добавить ПРОСЛУШИТЕЛИ 
+//чтобыя могла определять, что происходит,
+// когда я нажимаю кнопка добавления или вычитания
+counterPlusElem.addEventListener("click",()=>{ 
     count++;
     updateDisplay();
 }) ;
